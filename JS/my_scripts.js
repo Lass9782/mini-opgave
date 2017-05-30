810c89d5d0ff7923e0c1d2738ba8a4a7deead2e1
Lasse Bredahl Vangsgaard <lass9782@edu.campusvejle.dk>
2017-05-30 13:16:09 +0200

    Tilføjede clubBS

--- JS/my_scripts.js
+++ JS/my_scripts.js
@@ -3,9 +3,5 @@ $(function () {
     $('.fadein img:gt(0)').hide();
     setInterval(function () {
         $('.fadein img:first-child').fadeOut(3000).next('img').fadeIn(3000).end().appendTo('.fadein');
-<<<<<<< HEAD
     }, 5000);
-=======
-    }, 4000);
->>>>>>> 40f630bd9c1f12e9b3c6a19e0ba2844893585cec
 });
