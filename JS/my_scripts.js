5a2ec9f128896d9a358ee0d76027e74dddf4d271
Lasse Bredahl Vangsgaard <lass9782@edu.campusvejle.dk>
2017-05-30 10:49:47 +0200

    Tilføjede kontak

--- JS/my_scripts.js
+++ JS/my_scripts.js
@@ -3,5 +3,5 @@ $(function () {
     $('.fadein img:gt(0)').hide();
     setInterval(function () {
         $('.fadein img:first-child').fadeOut(3000).next('img').fadeIn(3000).end().appendTo('.fadein');
-    }, 2000);
+    }, 5000);
 });
